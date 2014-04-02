75aeeee71fc4c314fd098d1c1ddf24aa223799cb
Vincent Cheung <muse.vincent@gmail.com>
2014-04-02 09:02:30 +0800

    Merge 骏骏的代码，主要解决mouseover问题
    1. 用clearTimeout解决了mouseover延时的问题
    2. 执行前先搜索popover-show，发现立即变hide，解决了同时只应有一个show的问题
    3. 加入keyboard.js，快捷键初步可用。

--- js/test.js
+++ js/test.js
@@ -1,24 +1,45 @@
-
 // 记住以下先是javascript，然后是jQuery
 var timeout_hide, timeout_show;
+
 function popover_show(target){
   timeout_show = window.setTimeout(function(){
-    jQuery(target).find(".popover").removeClass("popover-hide").addClass("popover-show");
-  }, 400);
+    $(".popover-show").removeClass("popover-show").addClass("popover-hide");
+    $(target).find(".popover").removeClass("popover-hide").addClass("popover-show");
+  }, 300);
   window.clearTimeout(timeout_hide);
-}
 
 function popover_hide(target){
   timeout_hide = window.setTimeout(function(){
-    jQuery(target).find(".popover").removeClass("popover-show").addClass("popover-hide");
+    $(target).find(".popover").removeClass("popover-show").addClass("popover-hide");
   }, 400);
 }
 
-// 记住以上先是javascript，然后是jQuery
 
+
+//快捷键
+function hotkeys(){
+  KeyboardJS.on('1>2', function() {
+    popover_show($("#button12"));
+});
+  KeyboardJS.on('1', function() {
+    popover_show($("#button1"));
+});
+  KeyboardJS.on('2', function() {
+    popover_show($("#button2"));
+});
+//  KeyboardJS.on('left', function() {
+//     currentfocus = $(currentfocus).next();
+// //right为prev()
+// //全局变量currentfocus:必须在click，show之后，将currentfocus进行重新赋值，赋的值为一个元素
+//     popover_show($(currentfocus));
+// });
+  
+}
 
 $(function(){
-
+  window.onload = function(){
+    hotkeys();
+  }
   $(".test").mouseover(function(){
     popover_show($(this));
   });
